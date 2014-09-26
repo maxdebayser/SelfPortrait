04ea5c31add8ab2d78714a7a444d645b2ed7760e
Max de Bayser <mbayser@br.ibm.com>
2014-09-26 20:19:42 -0300

    Add more info in the copyright notice

--- src/utils/lua_utils.h
+++ src/utils/lua_utils.h
@@ -1,6 +1,8 @@
 /******************************************************************************
 * Copyright (C) 2012-2014 Maximilien de Bayser
 *
+* SelfPortrait API - http://github.com/maxdebayser/SelfPortrait
+*
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
 * "Software"), to deal in the Software without restriction, including
