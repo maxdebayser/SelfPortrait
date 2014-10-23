9649cc710799aa2e7b93bde09755234737b3647a
Max de Bayser <mbayser@br.ibm.com>
2014-10-23 17:50:05 -0200

    Restrict automatic comparability detection to non template types or registered template types
    Because SFINAE does not do the full instantiation of types
    comparable<vector<NonComparable>> was resulting in true, leading
    to compilation failures
    
    With the new scheme template class rules can selectively be added

--- src/reflection/typeutils.h
+++ src/reflection/typeutils.h
@@ -6,7 +6,17 @@
 #define TYPE_UTILS_H
 
 #include <type_traits>
-
+#include <vector>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <utility>
+#include <list>
+#include <string>
+#include <array>
+#include <tuple>
 
 template<bool B, typename U, typename V>
 struct Select {
@@ -19,11 +29,132 @@ struct Select<true, U, V>
 	typedef U type;
 };
 
-template<class T, class = decltype(std::declval<T>() == std::declval<T>() )>
-std::true_type  supports_equal_test(const T&);
-std::false_type supports_equal_test(...);
 
-template<class T> using comparable = decltype(supports_equal_test(std::declval<T>()));
+template<class... T>
+struct static_and;
+
+template<>
+struct static_and<std::true_type> {
+    typedef std::true_type type;
+};
+
+template<>
+struct static_and<std::false_type> {
+    typedef std::false_type type;
+};
+
+template<class H, class... T>
+struct static_and<H,T...> {
+    static_assert(std::is_same<H, std::true_type>::value || std::is_same<H, std::false_type>::value, "");
+    typedef typename Select<std::is_same<H, std::true_type>::value, typename static_and<T...>::type, std::false_type>::type type;
+};
+
+
+template<class T>
+struct equal_test_helper {
+    template<class U, class = decltype(std::declval<U>() == std::declval<U>() )>
+    static std::true_type  supports_equal_test(const U&);
+
+    static std::false_type supports_equal_test(...);
+};
+
+// disable all templates
+template<template <typename...> class templClass, typename... T>
+struct equal_test_helper<templClass<T...>> {
+    template <class U>
+    static std::false_type supports_equal_test(const U&);
+};
+
+// enable basic_string if value_type is comparable
+template<class T, class Traits, class Alloc>
+struct equal_test_helper<std::basic_string<T, Traits, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::basic_string<T, Traits, Alloc>&);
+};
+
+
+// enable vector if value_type is comparable
+template<class T, class Alloc>
+struct equal_test_helper<std::vector<T, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::vector<T, Alloc>&);
+};
+
+// enable list if value_type is comparable
+template<class T, class Alloc>
+struct equal_test_helper<std::list<T, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::list<T, Alloc>&);
+};
+
+// enable array if value_type is comparable
+template<class T, std::size_t N>
+struct equal_test_helper<std::array<T, N>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::array<T, N>&);
+};
+
+// enable set if value_type is comparable
+template<class T, class Comp, class Alloc>
+struct equal_test_helper<std::set<T, Comp, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::set<T, Comp, Alloc>&);
+};
+
+
+// enable set if value_type is comparable
+template<class T, class Comp, class Alloc>
+struct equal_test_helper<std::multiset<T, Comp, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::multiset<T, Comp, Alloc>&);
+};
+
+
+// enable set if value_type is comparable
+template<class T, class Hash, class Pred, class Alloc>
+struct equal_test_helper<std::unordered_set<T, Hash, Pred, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::unordered_set<T, Hash, Pred, Alloc>&);
+};
+
+
+// enable set if value_type is comparable
+template<class T, class Hash, class Pred, class Alloc>
+struct equal_test_helper<std::unordered_multiset<T, Hash, Pred, Alloc>> {
+    static decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))  supports_equal_test(const std::unordered_multiset<T, Hash, Pred, Alloc>&);
+};
+
+// enable pair if value_type is comparable
+template<class T, class S>
+struct equal_test_helper<std::pair<T, S>> {
+    static typename static_and<decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>())), decltype(equal_test_helper<S>::supports_equal_test(std::declval<S>()))>::type supports_equal_test(const std::pair<T, S>&);
+};
+
+// enable map if value_type is comparable
+template<class T, class S, class Comp, class Alloc>
+struct equal_test_helper<std::map<T, S, Comp, Alloc>> {
+    static typename static_and<decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>())), decltype(equal_test_helper<S>::supports_equal_test(std::declval<S>()))>::type supports_equal_test(const std::map<T, S, Comp, Alloc>&);
+};
+
+// enable map if value_type is comparable
+template<class T, class S, class Comp, class Alloc>
+struct equal_test_helper<std::multimap<T, S, Comp, Alloc>> {
+    static typename static_and<decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>())), decltype(equal_test_helper<S>::supports_equal_test(std::declval<S>()))>::type supports_equal_test(const std::multimap<T, S, Comp, Alloc>&);
+};
+
+// enable map if value_type is comparable
+template<class T, class S, class Hash, class Pred, class Alloc>
+struct equal_test_helper<std::unordered_map<T, S, Hash, Pred, Alloc>> {
+    static typename static_and<decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>())), decltype(equal_test_helper<S>::supports_equal_test(std::declval<S>()))>::type supports_equal_test(const std::unordered_map<T, S, Hash, Pred, Alloc>&);
+};
+
+
+// enable map if value_type is comparable
+template<class T, class S, class Hash, class Pred, class Alloc>
+struct equal_test_helper<std::unordered_multimap<T, S, Hash, Pred, Alloc>> {
+    static typename static_and<decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>())), decltype(equal_test_helper<S>::supports_equal_test(std::declval<S>()))>::type supports_equal_test(const std::unordered_multimap<T, S, Hash, Pred, Alloc>&);
+};
+
+// enable pair if value_type is comparable
+template<class... T>
+struct equal_test_helper<std::tuple<T...>> {
+    static typename static_and<decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()))...>::type supports_equal_test(const std::tuple<T...>&);
+};
+
+template<class T> using comparable = decltype(equal_test_helper<T>::supports_equal_test(std::declval<T>()));
 
 
 template<class T>
--- src/unit_test/type_utils_test.cpp
+++ src/unit_test/type_utils_test.cpp
@@ -4,6 +4,7 @@
 #include "str_conversion.h"
 #include <memory>
 #include <iostream>
+#include <vector>
 namespace {
 
     class NonComparable {
@@ -17,14 +18,54 @@ namespace {
         return true;
     }
 
+    template<class T, class B = bool>
+    class comparable_template {
+    public:
+        T t;
+    };
+    template<class T>
+    bool operator==(const comparable_template<T>& t1, const comparable_template<T>& t2) {
+        return  t1.t == t2.t;
+    }
 }
-
 
 
 void TypeUtilsTestSuite::testMeta()
 {
+    static_assert(comparable<int>::value == true, "");
+    static_assert(comparable<std::string>::value == true, "");
+
     static_assert(comparable<Comparable>::value == true, "");
     static_assert(comparable<NonComparable>::value == false, "");
+
+    static_assert(comparable<comparable_template<Comparable>>::value == false, "");
+    static_assert(comparable<comparable_template<NonComparable>>::value == false, "");
+
+    static_assert(comparable<std::vector<Comparable>>::value == true, "");
+    static_assert(comparable<std::vector<NonComparable>>::value == false, "");
+
+    // test recursion, fuck yeah!
+    static_assert(comparable<std::vector<std::vector<Comparable>>>::value == true, "");
+    static_assert(comparable<std::vector<std::vector<NonComparable>>>::value == false, "");
+
+    static_assert(comparable<std::pair<Comparable, Comparable>>::value == true, "");
+    static_assert(comparable<std::pair<NonComparable, NonComparable>>::value == false, "");
+
+    static_assert( std::is_same<std::false_type, static_and<std::false_type>::type>::value, "");
+    static_assert( std::is_same<std::true_type, static_and<std::true_type>::type>::value, "");
+
+    static_assert( std::is_same<std::true_type,  static_and<std::true_type,  std::true_type>::type>::value,  "");
+    static_assert( std::is_same<std::false_type, static_and<std::true_type,  std::false_type>::type>::value, "");
+    static_assert( std::is_same<std::false_type, static_and<std::true_type,  std::false_type>::type>::value, "");
+    static_assert( std::is_same<std::false_type, static_and<std::false_type, std::false_type>::type>::value, "");
+
+    static_assert(comparable<std::pair<Comparable, NonComparable>>::value == false, "");
+    static_assert(comparable<std::pair<NonComparable, Comparable>>::value == false, "");
+
+    static_assert(comparable<std::tuple<Comparable, Comparable>>::value == true, "");
+    static_assert(comparable<std::tuple<NonComparable, NonComparable>>::value == false, "");
+    static_assert(comparable<std::tuple<Comparable, NonComparable>>::value == false, "");
+    static_assert(comparable<std::tuple<NonComparable, Comparable>>::value == false, "");
 
     static_assert(convertible_to_string<int>() == true, "");
     static_assert(convertible_to_string<std::string>() == true, "");
